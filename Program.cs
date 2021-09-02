d5913249b945d329d19b67deebd42ab617e67dce
Tiago Rodrigues <isatiago098@gmail.com>
2021-09-02 17:37:15 -0300

    resoluçao exercicio usuario

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercicioususario
+{
+    class Program
+    { 
+         static void Main(string[] args)
+        { 
+            string nomeUsuario;
+
+            Console.WriteLine("Digite o seu  nome!");
+            nomeUsuario =Console.ReadLine();
+
+            Console.WriteLine($"Ola, {nomeUsuario}");
+        }
+    }
+}
